2817d045505e27313628f9e0c6c62af76bec69f1
Maksym Semenykhin <MaksymSemenykhin@users.noreply.github.com>
2017-10-26 15:46:12 +0300

    Update HTTPRequester.php

--- HTTPRequester.php
+++ HTTPRequester.php
@@ -33,6 +33,41 @@ class HTTPRequester {
     	return $response;
       
     }
+	
+    /**
+     * @description Make HTTP-PUT call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPput($url, array $params = [], array $headers = []) {
+		if(in_array('Content-Type: application/json',$headers)){
+			$query = json_encode($params);    
+		}else{
+			$query = http_build_query($params);
+		}
+        $ch    = curl_init();
+		
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);		
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_PUT, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+		
+		if (defined('HTTPRVERBOSE') && HTTPRVERBOSE) {
+			curl_setopt($ch, CURLOPT_VERBOSE, true);
+#			$verbose = fopen('./curl.log', 'w+');
+#			curl_setopt($ch, CURLOPT_STDERR, $verbose);
+		}		
+		
+	  	$response = curl_exec($ch);
+    	curl_close($ch);
+    	return $response;
+      
+    }	
+	
+	
 }
 
 ?>
